a171da594c575081fb364d52d1f2bbc05f473a5c
AndreaGarofalo <andreasjobs94@outlook.it>
2023-02-23 17:45:23 +0100

    added various pages

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
+<body class="text-center">
     <h1>Hello World</h1>
+    <ul>
+        <li><a href="{{ route('home') }}">home</a></li>
+        <li><a href="{{ route('news') }}">news</a></li>
+        <li><a href="{{ route('contact') }}">contact</a></li>
+        <li><a href="{{ route('support') }}">support</a></li>
+    </ul>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
+Route::get('/news', function () {
+    return view('news');
+})->name('news');
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+Route::get('/support', function () {
+    return view('support');
+})->name('support');
